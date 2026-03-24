6567d402207f06157e39d302c54743905de20a29
Artem Ismagilov <artem.ismagilov.2000@mail.ru>
2026-03-24 11:13:18 +0400

    use String fuzz

--- fuzz/fuzz_targets/fuzz_target_3.rs
+++ fuzz/fuzz_targets/fuzz_target_3.rs
@@ -6,19 +6,15 @@ use smol::io::Cursor;
 
 use mcmc_rs::version_cmd;
 
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: String| {
     // fuzzed code goes here
     block_on(async {
-        if data.is_empty()
-            || data.contains(&b'\n')
-            || data.contains(&b'\r')
-            || String::from_utf8(data.to_vec()).is_err()
-        {
+        if data.is_empty() || data.contains("\n") || data.contains("\r") {
             return;
         }
         // version\r\nVERSION 1.2.3\r\n
         let mut w = Vec::from(b"version\r\nVERSION ");
-        w.extend(data);
+        w.extend(data.as_bytes());
         w.extend(b"\r\n");
 
         version_cmd(&mut Cursor::new(w)).await.unwrap();
